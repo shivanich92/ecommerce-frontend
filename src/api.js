01d8e2a2b428f1c76f9c0ea163478981cd7ad894
2203a52078 <158256245+shivanich92@users.noreply.github.com>
2025-09-06 14:39:16 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000';
+export const API_BASE = import.meta.env.VITE_API_BASE || 'https://ecommerce-backend-ovyb.onrender.com';
+
 export const api = axios.create({ baseURL: API_BASE });
 api.interceptors.request.use((config)=>{
   const token = localStorage.getItem('token');
